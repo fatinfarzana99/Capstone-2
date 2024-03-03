a99f5191c1d1f774e2cbf982569c0e15cd53b9a0
fatinfarzana <rosettafarzana@gmail.com>
2024-03-04 00:47:40 +0800

    Week 8 progress.

--- V01.03/linear actuator/motor_control/motor_control.ino
+++ V01.03/linear actuator/motor_control/motor_control.ino
@@ -1,7 +1,7 @@
 //DC MOTOR CONTROL
 
-int in1 = 9; //Declaring the pins where in1 in2 from the driver are wired 
-int in2 = 8; //here they are wired with D9 and D8 from Arduino
+int in1 = 5; //Declaring the pins where in1 in2 from the driver are wired 
+int in2 = 4; //here they are wired with D9 and D8 from Arduino
 void setup() {
   pinMode(in1, OUTPUT); //Declaring the pin modes, obviously they're outputs
   pinMode(in2, OUTPUT);
@@ -14,8 +14,13 @@ void TurnOFFA(){
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
 }
+void TurnMotorAcw(){              
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+}
 void loop() {
-  TurnMotorA(); //in the loop we use the function to turn the motor for 3s and stop it for 2s
+  //TurnMotorA(); //in the loop we use the function to turn the motor for 3s and stop it for 2s
+  TurnMotorAcw();
   //delay(3000);
   //TurnOFFA();
   //delay(2000);
