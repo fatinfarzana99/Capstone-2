7021e2c94fcf03cd7a9a5a9551e508a256e70a7a
fatinfarzana <rosettafarzana@gmail.com>
2024-03-23 16:36:55 +0800

    almost there

--- V01.07/finalsketch/finalsketch.ino
+++ V01.07/finalsketch/finalsketch.ino
@@ -1,6 +1,10 @@
 #include <Servo.h>
 #include <AccelStepper.h>
 #include <NewPing.h>
+#include <ezButton.h>
+
+ezButton limitSwitch(50);  // create ezButton object that attach to pin 7;
+ezButton limitSwitch2(52);  // create ezButton object that attach to pin 7;
 
 Servo GRIPPER;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
@@ -21,12 +25,20 @@ AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN_STEPPER_MOTOR, DIR_PIN_STEPP
 
 bool linearActuatorForwardFlag = false;
 bool linearActuatorBackwardFlag = false;
+bool PlatformDown = false;
 
 #define TRIGGER_PIN_1  12
 #define ECHO_PIN_1     13
 #define TRIGGER_PIN_2  10  // Change to the pin number you're using for the second ultrasonic sensor
 #define ECHO_PIN_2     11  // Change to the pin number you're using for the second ultrasonic sensor
 #define MAX_DISTANCE 400 // Maximum distance we want to measure (in centimeters).
+#define PING_INTERVAL 20 // Set the desired ping interval in milliseconds (e.g., 20ms for approximately 50 pings per second)
+unsigned long lastPingTime = 0; // Variable to store the time of the last ping
+
+
+
+#define LIMITSWITCH_1 50 // Change this to the appropriate pin
+#define LIMITSWITCH_2 52 // Change this to the appropriate pin
 
 NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE); // NewPing setup of pins and maximum distance for sensor 1
 NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE); // NewPing setup of pins and maximum distance for sensor 2
@@ -47,19 +59,64 @@ void setup() {
   stepper.setAcceleration(500);  // Adjust as needed
 
   GRIPPER.attach(9);
+
+  limitSwitch.setDebounceTime(50); // set debounce time to 50 milliseconds
+  limitSwitch2.setDebounceTime(50); // set debounce time to 50 milliseconds
+
+}
+void LimitSwitches()
+{
+  limitSwitch.loop(); // MUST call the loop() function first
+  limitSwitch2.loop(); // MUST call the loop() function first
+
+  /***********************LIMIT SWITCH 1*****************************/
+  if(limitSwitch.isPressed())
+    Serial.println("The limit switch 1: UNTOUCHED -> TOUCHED");
+
+  if(limitSwitch.isReleased())
+    Serial.println("The limit switch 1: TOUCHED -> UNTOUCHED");
+
+  int state = limitSwitch.getState();
+  if(state == HIGH)
+    Serial.println("The limit switch 1: UNTOUCHED");
+  else
+    Serial.println("The limit switch 1: TOUCHED");
+
+  /***********************LIMIT SWITCH 2*****************************/
+  if(limitSwitch2.isPressed())
+    Serial.println("The limit switch 2: UNTOUCHED -> TOUCHED");
+
+  if(limitSwitch2.isReleased())
+    Serial.println("The limit switch 2: TOUCHED -> UNTOUCHED");
+
+  int state2 = limitSwitch2.getState();
+  if(state2 == HIGH)
+    Serial.println("The limit switch 2: UNTOUCHED");
+  else
+    Serial.println("The limit switch 2: TOUCHED");
 }
 
  /***************************************************************************************/
  /**********************************AC MOTOR*********************************************/
 /****************************************************************************************/
-void RotatePlatformEvent()
-{
+void RotatePlatformUpEvent()
+{   
     digitalWrite(RELAY_PIN_IN1_AC_MOTOR, HIGH);
     digitalWrite(RELAY_PIN_IN2_AC_MOTOR, LOW);
 
-    delay(10000);
+    //delay(10000);
 
-    StopPlatformRotation();
+    //StopPlatformRotation();
+}
+
+void RotatePlatformDownEvent()
+{   
+    digitalWrite(RELAY_PIN_IN1_AC_MOTOR, LOW);
+    digitalWrite(RELAY_PIN_IN2_AC_MOTOR, HIGH);
+
+    //delay(10000);
+
+    //StopPlatformRotation();
 }
 
 void StopPlatformRotation()
@@ -78,8 +135,8 @@ void LinearActuatorBackward()
     digitalWrite(RELAY_PIN_IN3_ACTUATOR, HIGH);
     digitalWrite(RELAY_PIN_IN4_ACTUATOR, LOW);
 
-    delay(60000); // Delay for 1 minute
-    delay(43000); // Delay for 43 seconds // wait for a second
+    //delay(60000); // Delay for 1 minute
+    //delay(43000); // Delay for 43 seconds // wait for a second
     
     linearActuatorBackwardFlag = true;
 }
@@ -88,10 +145,8 @@ void LinearActuatorForward()
     digitalWrite(RELAY_PIN_IN3_ACTUATOR, LOW);
     digitalWrite(RELAY_PIN_IN4_ACTUATOR, HIGH);
 
-    delay(60000); // Delay for 1 minute
-    delay(43000); // Delay for 43 seconds // wait for a second
-
-    linearActuatorForwardFlag = true;
+    //delay(60000); // Delay for 1 minute
+    //delay(43000); // Delay for 43 seconds // wait for a second
 }
 void LinearActuatorOff()
 {
@@ -106,7 +161,7 @@ void LinearActuatorOff()
 void LinearActuatorGoDown()
 {
   // Set the speed at which the stepper motor will rotate
-  stepper.setSpeed(-100); // Adjust as needed
+  stepper.setSpeed(-300); // Adjust as needed
   // Rotate the motor continuously
   stepper.runSpeed();
 
@@ -115,7 +170,15 @@ void LinearActuatorGoDown()
 void LinearActuatorGoUp()
 {
   // Set the speed at which the stepper motor will rotate
-  stepper.setSpeed(100); // Adjust as needed
+  stepper.setSpeed(300); // Adjust as needed
+  // Rotate the motor continuously
+  stepper.runSpeed();
+}
+
+void LinearActuatorGoStop()
+{
+  // Set the speed at which the stepper motor will rotate
+  stepper.setSpeed(0); // Adjust as needed
   // Rotate the motor continuously
   stepper.runSpeed();
 }
@@ -160,34 +223,67 @@ void RetrieveTrayEvent()
 }
 
  /***********************************************************************************/
-void gripperTest()
+void gripperOpen()
 {
-    for (pos = 0; pos <= 180; pos += 1) { 
-    GRIPPER.write(pos); // move the servo to the current position
-    delay(15); // wait for the servo to reach the position
-  }
-  
-  // Sweep from 180 to 0 degrees
+
+        // Sweep from 180 to 0 degrees
   for (pos = 180; pos >= 0; pos -= 1) {
-    GRIPPER.write(pos); // move the servo to the current position
-    delay(15); // wait for the servo to reach the position
+      GRIPPER.write(pos); // move the servo to the current position
+      delay(15); // wait for the servo to reach the position
   }
+  /*while(1)
+  {
 
-    for (pos = 0; pos <= 180; pos += 1) { 
-    GRIPPER.write(pos); // move the servo to the current position
-    delay(15); // wait for the servo to reach the position
-  }
-
-    // Sweep from 180 to 0 degrees
-  for (pos = 180; pos >= 0; pos -= 1) {
-    GRIPPER.write(pos); // move the servo to the current position
-    delay(15); // wait for the servo to reach the position
-  }
-
-while(1)
-{
+  }*/
 
 }
+
+void gripperClose()
+{
+      for (pos = 0; pos <= 180; pos += 1) { 
+    GRIPPER.write(pos); // move the servo to the current position
+    delay(15); // wait for the servo to reach the position
+    }
+
+    /*while(1)
+    {
+
+    }*/
+}
+
+
+void StopLinearActuatorAndBelt()
+{
+
+
+
+  if(limitSwitch2.isPressed())
+    Serial.println("The limit switch 2: UNTOUCHED -> TOUCHED");
+
+  if(limitSwitch2.isReleased())
+    Serial.println("The limit switch 2: TOUCHED -> UNTOUCHED");
+
+  int state2 = limitSwitch2.getState();
+  if(state2 == HIGH)
+  {    Serial.println("The limit switch 2: UNTOUCHED");
+       
+       //LinearActuatorGoUp();
+       //LinearActuatorGoStop();
+      // RotatePlatformDownEvent();
+      LinearActuatorGoStop();
+  }
+
+  else
+  {
+    Serial.println("The limit switch 2: TOUCHED");
+    LinearActuatorGoDown();
+    //StopPlatformRotation();
+
+    
+  }
+
+ // 
+
 }
  /***********************************************************************************/
   
@@ -203,106 +299,99 @@ void loop() {
   //LinearActuatorForward();
 
   //LinearActuatorGoDown();
-  LinearActuatorGoUp();
+  //LinearActuatorGoUp();
 
-  //gripperTest();
 
-  /*************US SENSOR**/
-  /*delay(50);  // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
 
-  int distance1 = sonar1.ping_cm(); // Send ping from sensor 1, get distance in cm
-  int distance2 = sonar2.ping_cm(); // Send ping from sensor 2, get distance in cm
+  //delay(1000);
 
-  Serial.print("Distance Sensor 1: ");
-  Serial.print(distance1);
-  Serial.println("cm");
+  //gripperClose();
+  /*************US SENSOR******************************************************/
+    /*unsigned long currentTime = millis();
+    int distance1 = sonar1.ping_cm(); // Send ping from sensor 1, get distance in cm
+    int distance2 = sonar2.ping_cm(); // Send ping from sensor 2, get distance in cm
+    // Check if it's time to send another ping
+    if (currentTime - lastPingTime >= PING_INTERVAL) {
+      // Send pings from both sensors
 
-  Serial.print("Distance Sensor 2: ");
-  Serial.print(distance2);
-  Serial.println("cm");
+      // Print distances
+      Serial.print("Distance Sensor 1: ");
+      Serial.print(distance1);
+      Serial.println("cm");
 
-  if((distance1 <= 10) && (distance2 <= 10))
+      Serial.print("Distance Sensor 2: ");
+      Serial.print(distance2);
+      Serial.println("cm");
+
+      // Update last ping time
+      lastPingTime = currentTime;
+    }
+
+    if((distance1 <= 10) && (distance2 <= 10))
+    {
+        gripperClose();
+        delay(1000);
+        //LinearActuatorBackward();
+        linearActuatorBackwardFlag = true;
+    }
+
+    else
+    {
+      gripperOpen();
+      //LinearActuatorForward();
+      //LinearActuatorBackward();
+      linearActuatorForwardFlag = true;
+    }
+
+    if(linearActuatorBackwardFlag == true)
+    {
+      LinearActuatorBackward();
+      PlatformDown = true;
+      linearActuatorBackwardFlag = false;
+    }
+
+    else if(linearActuatorBackwardFlag == false)
+    {
+      LinearActuatorForward();
+    }
+    
+    if(PlatformDown == true)
+    {
+      StopLinearActuatorAndBelt();
+      PlatformDown = false;
+    }*/
+    limitSwitch.loop(); // MUST call the loop() function first
+    limitSwitch2.loop(); // MUST call the loop() function first
+
+  if(limitSwitch.isPressed())
+    Serial.println("The limit switch 1: UNTOUCHED -> TOUCHED");
+
+  if(limitSwitch.isReleased())
+    Serial.println("The limit switch 1: TOUCHED -> UNTOUCHED");
+
+  int state = limitSwitch.getState();
+  if(state == HIGH)
   {
-        for (pos = 0; pos <= 180; pos += 1) 
-        { 
-          GRIPPER.write(pos); // move the servo to the current position
-          delay(15); // wait for the servo to reach the position
-        }
-        while(1)
-        {
-
-        }
-
-  }*/
-
-
-
-}
-
-
-
-
-/*#include <Servo.h>
-
-// Pin definitions for Ultrasonic Sensor 1
-#define TRIG_PIN_US_SENSOR 12
-#define ECHO_PIN_US_SENSOR 13
-
-// Pin definitions for Ultrasonic Sensor 2
-#define TRIG_PIN_US_SENSOR_2 10
-#define ECHO_PIN_US_SENSOR_2 11
-
-// Define servo objects
-Servo myservo;  
-
-
-// Function to read distance from Ultrasonic Sensor 1
-long readDistanceFromSensor1() {
-  digitalWrite(TRIG_PIN_US_SENSOR, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN_US_SENSOR, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN_US_SENSOR, LOW);
-  return pulseIn(ECHO_PIN_US_SENSOR, HIGH) / 2 / 29.1;
-}
-
-// Function to read distance from Ultrasonic Sensor 2
-long readDistanceFromSensor2() {
-  digitalWrite(TRIG_PIN_US_SENSOR_2, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN_US_SENSOR_2, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN_US_SENSOR_2, LOW);
-  return pulseIn(ECHO_PIN_US_SENSOR_2, HIGH) / 2 / 29.1;
-}
-
-void setup() {
-  pinMode(TRIG_PIN_US_SENSOR, OUTPUT);
-  pinMode(ECHO_PIN_US_SENSOR, INPUT);
-  pinMode(TRIG_PIN_US_SENSOR_2, OUTPUT);
-  pinMode(ECHO_PIN_US_SENSOR_2, INPUT);
-  
-  // Attach servos to pins
-  myservo.attach(9);
-
-  // Initialize servos position
-  myservo.write(0);
-
-}
-
-void loop() {
-  Serial.begin(9600);
-  long distance1 = readDistanceFromSensor1();
-  long distance2 = readDistanceFromSensor2();
-        Serial.print(distance1);
-      Serial.println(" cm");
-            //Serial.print(distance2);
-      //Serial.println(" cm");
-  if (distance1 <= 10) {
-    myservo.write(180);
-  } else if (distance2 <= 10) {
-    myservo.write(0);
+    Serial.println("The limit switch 1: UNTOUCHED");
+    //RotatePlatformUpEvent();
+    //RotatePlatformDownEvent();
+    //StopPlatformRotation();
+    LinearActuatorGoStop();
   }
 
-}*/
+  else
+  {
+    Serial.println("The limit switch 1: TOUCHED");  
+    //StopPlatformRotation();
+    
+    LinearActuatorGoUp();
+  }
+  
+ // StopLinearActuatorAndBelt();
+
+
+
+}
+
+
 
